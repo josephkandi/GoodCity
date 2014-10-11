2282dca7c4798f9b771f221ad85c4bf76a45c6b7
Yili Aiwazian <yili.aiwazian@gmail.com>
2014-10-10 22:53:58 -0700

    new files, etc

---
+++ GoodCity/ContainerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ContainerViewController.swift
+//  GoodCity
+//
+//  Created by Yili Aiwazian on 10/10/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    @IBOutlet weak var containerScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        //let containerViewController = ContainerViewController(nibName: "ContainerViewController", bundle: nil)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
