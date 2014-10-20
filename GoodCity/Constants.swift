f6e1a7c3e462c556c615049fddd55ca96e2e1902
Yili Aiwazian <yili.aiwazian@gmail.com>
2014-10-19 22:50:16 -0700

    Custom date picker drop down button

--- GoodCity/Constants.swift
+++ GoodCity/Constants.swift
@@ -8,12 +8,14 @@
 
 import Foundation
 
+let ROUNDED_CORNER = CGFloat(4)
 let SPACING = CGFloat(5)
 let BUTTON_SPACING = CGFloat(8)
 let BUTTON_HEIGHT = CGFloat(40)
 let SECTION_HEADER_HEIGHT = CGFloat(36)
 
 let FONT_14 = UIFont(name: "Avenir Next", size: 14.0)
+let FONT_15 = UIFont(name: "Avenir Next", size: 15.0)
 let FONT_MEDIUM_12 = UIFont(name: "AvenirNext-Medium", size: 12.0)
 let FONT_MEDIUM_14 = UIFont(name: "AvenirNext-Medium", size: 14.0)
 let FONT_MEDIUM_20 = UIFont(name: "AvenirNext-Medium", size: 20.0)
@@ -22,6 +24,7 @@ let FONT_BOLD_18 = UIFont(name: "AvenirNext-Bold", size: 18.0)
 
 // Colors
 let LIGHT_GRAY_BG = UIColorFromRGB(0xf0f0f0)
+let GRAY_TEXT = UIColorFromRGB(0x9B9B9B)
 let FB_BLUE = UIColor(red: 0.278, green: 0.3843, blue: 0.6078, alpha: 1.0)
 let DARK_GRAY = UIColor(white: 0.25, alpha: 1.0)
 let BLUE_TEAL = UIColorFromRGB(0x03b7e7)
---
+++ GoodCity/DatePickerButton.swift
@@ -0,0 +1,66 @@
+//
+//  RoundedButton.swift
+//  GoodCity
+//
+//  Created by Yili Aiwazian on 10/12/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import UIKit
+
+let BUTTON_INSET = CGFloat(12)
+let ICON_SIZE = CGFloat(15)
+
+class DatePickerButton: UIButton {
+    
+    var dropdownIcon : UIImageView!
+
+    override init() {
+        super.init()
+        setup()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    func setup() {
+        self.layer.backgroundColor = UIColor.whiteColor().CGColor
+        self.layer.cornerRadius = ROUNDED_CORNER
+        self.layer.masksToBounds = true
+        
+        self.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Left
+        self.contentEdgeInsets = UIEdgeInsetsMake(0, BUTTON_INSET, 0, 0)
+        
+        dropdownIcon = UIImageView(image: UIImage(named: "schedule_dropdown").imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate))
+        dropdownIcon.tintColor = UIColor.lightGrayColor()
+        dropdownIcon.contentMode = UIViewContentMode.ScaleAspectFill
+        self.addSubview(dropdownIcon)
+        
+        if (self.titleLabel!.text != nil) {
+            self.setAttributedTitle(formatTitle(self.titleLabel!.text!), forState: .Normal)
+        }
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let bounds = self.bounds
+        dropdownIcon.frame = CGRectMake(bounds.width-BUTTON_INSET-ICON_SIZE, (bounds.height-ICON_SIZE)/2, ICON_SIZE, ICON_SIZE)
+    }
+    func setButtonTitle(text: String) {
+        self.setAttributedTitle(formatTitle(text), forState: .Normal)
+    }
+    func setButtonColor(color: UIColor) {
+        self.layer.backgroundColor = color.CGColor
+    }
+    
+    func formatTitle(text: String) -> NSMutableAttributedString {
+        let attributedTitle = NSMutableAttributedString(string: text)
+        let range = NSMakeRange(0, attributedTitle.length)
+        attributedTitle.addAttribute(NSFontAttributeName, value: FONT_15, range: range)
+        attributedTitle.addAttribute(NSForegroundColorAttributeName, value: UIColor.darkTextColor(), range: range)
+        return attributedTitle
+    }
+}
