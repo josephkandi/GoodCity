a29610d1a4c9ad1a61b5a29be6940abfda631844
Yili Aiwazian <yili.aiwazian@gmail.com>
2014-10-22 03:47:33 -0700

    Edit item view styling - round 2

--- GoodCity/EditItemView.swift
+++ GoodCity/EditItemView.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
+private let marginTopBottom: CGFloat = 30
+private let marginLeftRight: CGFloat = 20
+private let gapMargin: CGFloat = 20
+
 class EditItemView: UIView, UITextViewDelegate {
 
     @IBOutlet weak var descriptionText: UITextView!
     @IBOutlet weak var line: UIView!
-
     @IBOutlet weak var conditionChooser: YASegmentedControl!
     
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var closeButton: UIButton!
     var delegate: CameraViewDelegate?
     var photo: UIImage?
 
+    var itemDescription = ""
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -29,15 +39,11 @@ class EditItemView: UIView, UITextViewDelegate {
     override func awakeFromNib() {
         // Set up the text compose field
         descriptionText.delegate = self
+        descriptionText.textAlignment = .Center
+        formatTextView(false)
 
         line.backgroundColor = UIColor(white: 0.1, alpha: 0.7)
-        descriptionText.textColor = offWhiteColor
-        descriptionText.alpha = 0.5
-        descriptionText.font = FONT_MEDIUM_14
-        descriptionText.textAlignment = .Center
-        descriptionText.text = "What is it? How many items are there?\nHow big is it?"
     }
-    
     
     @IBAction func onTap(sender: UITapGestureRecognizer) {
         descriptionText.resignFirstResponder()
@@ -55,12 +61,86 @@ class EditItemView: UIView, UITextViewDelegate {
     }
     
     override func layoutSubviews() {
+        
+        if descriptionText.isFirstResponder() {
+            blurView.frame = CGRectMake(0, 0, self.frame.width, self.frame.height-200)
+            descriptionText.frame = CGRectMake(marginLeftRight, blurView.frame.height/2-descriptionText.frame.height, blurView.frame.width - 2 * marginLeftRight, descriptionText.frame.height)
+            line.alpha = 0
+            conditionChooser.alpha = 0
+            closeButton.alpha = 0
+            submitButton.alpha = 0
+            blurView.alpha = 1
+        }
+        else {
+            var yOffset = marginTopBottom
+            descriptionText.sizeToFit()
+            descriptionText.frame = CGRectMake(marginLeftRight, yOffset, blurView.frame.width-marginLeftRight*2, descriptionText.frame.height)
+            
+            yOffset += descriptionText.frame.height + 5
+            line.frame = CGRectMake(marginLeftRight,yOffset, blurView.frame.width-marginLeftRight*2, 2)
+            
+            yOffset += gapMargin + line.frame.height
+            conditionChooser.frame = CGRectMake(marginLeftRight,yOffset, blurView.frame.width-marginLeftRight*2, 50)
+            
+            yOffset += conditionChooser.frame.height + marginTopBottom
+            blurView.frame = CGRectMake(0, self.frame.height - yOffset, self.frame.width, yOffset)
+            
+            blurView.alpha = 0.7
+            closeButton.alpha = 1
+            submitButton.alpha = 1
+            line.alpha = 1
+            conditionChooser.alpha = 1
+        }
+        
+
+    }
+    
+    private func formatTextView(active: Bool) {
+        descriptionText.sizeToFit()
+        if !active {
+            descriptionText.textColor = offWhiteColor
+            descriptionText.alpha = 0.5
+            descriptionText.font = FONT_MEDIUM_14
+        }
+        else {
+            descriptionText.font = FONT_MEDIUM_20
+            descriptionText.textColor = UIColor.whiteColor()
+        }
+        
+        if itemDescription == "" && !active {
+            showPlaceholderText()
+        }
+        else {
+            descriptionText.text = itemDescription
+        }
+    }
+    func dismissKeyboard() {
+        itemDescription = descriptionText.text
+        formatTextView(false)
+
+        descriptionText.resignFirstResponder()
+
+        layoutSubviews()
+    }
+
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == "\n" {
+            dismissKeyboard()
+            return false
+        }
+        return true
     }
     
     func textViewDidBeginEditing(textView: UITextView) {
+        
+        formatTextView(true)
         descriptionText.becomeFirstResponder()
-        descriptionText.textColor = UIColor.whiteColor()
-        descriptionText.text = ""
+        layoutSubviews()
+    }
+    
+    private func showPlaceholderText() {
+        descriptionText.text = "What is it? How many items are there?\nHow big is it?"
     }
 
 }
