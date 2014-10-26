b1987360a535cf7583370bcfb6dba44446bd9ac5
Nick Aiwazian <nick@shopular.com>
2014-10-25 19:22:21 -0700

    Today widget wired up with data

--- GoodCityWidget/TodayViewController.swift
+++ GoodCityWidget/TodayViewController.swift
@@ -9,50 +9,57 @@
 import UIKit
 import NotificationCenter
 
+let TOTAL_DONATION_VALUE_KEY = "total_donation_value"
+let TOTAL_DONATION_COUNT_KEY = "total_donation_count"
+let MEMBER_SINCE_KEY = "member_since"
+
 class TodayViewController: UIViewController, NCWidgetProviding {
-    @IBOutlet weak var donationsValueLabel: UILabel!
+
+    @IBOutlet weak var donationCountLabel: UILabel!
+    @IBOutlet weak var donationValueLabel: UILabel!
+    @IBOutlet weak var memberSinceLabel: UILabel!
+    @IBOutlet weak var badgeImage: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         println("In viewDidLoad...")
-        // Fetch network info
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "launchApp")
-        self.view.addGestureRecognizer(tapGestureRecognizer)
-        let userDefaults = NSUserDefaults(suiteName: "group.com.codepath.goodcity")
-        if let value = userDefaults?.valueForKey("total_donation_value") as? Double {
-            println("Value: \(value)")
-            self.donationsValueLabel.text = NSString(format: "$%.2f", value)
-        }         //self.donationsValueLabel.text = NSString(format: "$%.2f", 300)
-        //        userDefaults?.setDouble(result as Double, forKey: "total_donation_value")
 
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: "launchContainingApp")
+        self.view.addGestureRecognizer(tapGestureRecognizer)
+
+        self.updateFromUserDefaults()
     }
 
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         println("In widgetPerformUpdateWithCompletionHandler...")
 
-        let userDefaults = NSUserDefaults(suiteName: "group.com.codepath.goodcity")
-        if let value = userDefaults?.valueForKey("total_donation_value") as? Double {
-            println("Value: \(value)")
-            self.donationsValueLabel.text = NSString(format: "$%.2f", value)
-        }         //self.donationsValueLabel.text = NSString(format: "$%.2f", 300)
+        self.updateFromUserDefaults()
 
-        // Perform any setup necessary in order to update the view.
+        // TODO: Also fetch results from network in case items were approved since app was last opened
+        completionHandler(NCUpdateResult.NoData)
+    }
 
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-
-        completionHandler(NCUpdateResult.NewData)
+    func updateFromUserDefaults() {
+        if let userDefaults = NSUserDefaults(suiteName: "group.com.codepath.goodcity") {
+            if let donationValue = userDefaults.valueForKey(TOTAL_DONATION_VALUE_KEY) as? Double {
+                self.donationValueLabel.text = NSString(format: "$%.2f", donationValue)
+            }
+            if let donationCount = userDefaults.valueForKey(TOTAL_DONATION_COUNT_KEY) as? Int {
+                self.donationCountLabel.text = "\(donationCount)"
+            }
+            if let memberSince = userDefaults.objectForKey(MEMBER_SINCE_KEY) as? String {
+                self.memberSinceLabel.text = memberSince
+            }
+        }
     }
 
     func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
         return UIEdgeInsetsZero
     }
 
-    func launchApp() {
+    func launchContainingApp() {
         println("Registered tap...launching app")
         let url = NSURL(string: "goodcity://widget")
         self.extensionContext?.openURL(url!, completionHandler: nil)
     }
-    
 }
