6542acc9ef8deacbd10b8bddcdc0f2cc14e1d56e
Nick Aiwazian <nick@shopular.com>
2014-11-02 18:39:59 -0800

    Double extensions

---
+++ GoodCity/Double+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Double+Extensions.swift
+//  GoodCity
+//
+//  Created by Nick Aiwazian on 11/1/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+
+    static func degreesToRadians (value:Double) -> Double {
+        return value * M_PI / 180.0
+    }
+
+    static func radiansToDegrees (value:Double) -> Double {
+        return value * 180.0 / M_PI
+    }
+
+    static func preserveSignMod(a: Double, _ b: Double) -> Double {
+        return (a % b + b) % b
+    }
+}
